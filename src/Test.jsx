2969f18248d488a68b5a903e7b6b54f668f31382
Shin <sinsue2208@naver.com>
2023-04-20 16:57:53 +0900

    init

---
+++ src/Test.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+  //변수와 변수를 설정해주는함수
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={() => setCount(count + 1)}>{count}</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
